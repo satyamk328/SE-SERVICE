c7331795c97b544548faf45e3d246fe570355846
Satyam Kumar <satyam.kumar@agreeya.net>
2019-03-11 19:00:16 +0530

    Initially commited

---
+++ src/main/java/school_erp_service/school_erp_service/App.java
@@ -0,0 +1,13 @@
+package school_erp_service.school_erp_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
