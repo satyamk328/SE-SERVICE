b05e8ad3ae7a7e9452d874710cae7e0c361c6ef3
satyam.kumar <satyam.kumar@GDC-SatyamK-LPT.agreeya.local>
2019-03-29 21:15:04 +0530

    aaaaaaaaaaa

--- school-user/src/main/java/com/config/UserConfig.java
+++ school-user/src/main/java/com/config/UserConfig.java
@@ -1,10 +1,20 @@
 package com.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @PropertySource({"classpath:sql.properties" })
 public class UserConfig {
 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+		configurer.setIgnoreUnresolvablePlaceholders(true);
+		configurer.setIgnoreResourceNotFound(true);
+		return configurer;
+	}
+	
 }
