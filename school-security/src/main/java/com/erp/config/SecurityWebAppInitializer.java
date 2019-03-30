d084645b435397820ab268d1eb506cb4b4b808a0
satyam.kumar <satyam.kumar@GDC-SatyamK-LPT.agreeya.local>
2019-03-30 15:50:45 +0530

    ffff

---
+++ school-security/src/main/java/com/erp/config/SecurityWebAppInitializer.java
@@ -0,0 +1,7 @@
+package com.erp.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
